eff164fbd1bf8092a29ac2b1ce81f1997bee93f0
marvinick <marviluw@gmail.com>
2014-01-15 15:14:38 -0600

    added shifts

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -69,6 +69,6 @@ class SchedulesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def schedule_params
-      params[:schedule]
+      params.require(:schedule).permit(:day, :shift)
     end
 end
