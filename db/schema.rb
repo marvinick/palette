6d15a53a714ec4b6485bd35763a171f63d74d3e2
marvinick <marviluw@gmail.com>
2014-01-18 14:14:27 -0600

    posts routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140115183554) do
+ActiveRecord::Schema.define(version: 20140117215237) do
 
   create_table "employees", force: true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "description"
