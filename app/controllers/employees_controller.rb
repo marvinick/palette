7fe5e9499d0d74e38ec403a758afac234a16ece5
marvinick <marviluw@gmail.com>
2014-02-10 17:57:35 -0600

    employees_controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,12 +13,25 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    @employee = Employee.create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      flash[:notice] = "You just added a new employee"
+      redirect_to employees_path
+    else
+      render :form
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :title, :description)
   end
 end
